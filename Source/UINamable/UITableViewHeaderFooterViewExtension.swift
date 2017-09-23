76f66338ec41edf2e312c9f6153abfadf6945a99
Rodolfo Antonici <rodolfoantonici@MacBook-Pro-de-Rodolfo.local>
2017-09-23 13:28:32 -0300

    Added UITableView related classes

---
+++ Source/UINamable/UITableViewHeaderFooterViewExtension.swift
@@ -0,0 +1,29 @@
+//
+//  UITableViewHeaderFooterViewExtension.swift
+//  POC4VEngine
+//
+//  Created by Rodolfo Antonici on 23/09/17.
+//  Copyright © 2017 Rodolfo Antonici. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewHeaderFooterView {
+    public struct Identifier: RawRepresentable, Equatable, Hashable {
+        
+        public let rawValue: String
+        public var hashValue: Int {
+            return rawValue.hashValue
+        }
+        
+        //RawRepresentable
+        public init?(rawValue: String) {
+            self.rawValue = rawValue
+        }
+        
+        //Equatable
+        public static func ==(lhs: UITableViewHeaderFooterView.Identifier, rhs: UITableViewHeaderFooterView.Identifier) -> Bool {
+            return lhs.rawValue == rhs.rawValue
+        }
+    }
+}
---
+++ Source/UINamable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UINamable
+//
+//  Created by Rodolfo Antonici on 23/09/17.
+//  Copyright © 2017 Rodolfo Antonici. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
